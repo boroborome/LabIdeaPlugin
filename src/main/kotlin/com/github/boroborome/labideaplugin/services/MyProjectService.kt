3f4443b81655b9480a815e1f770b31ecadd36f62
GitHub Action <action@github.com>
2020-09-18 01:34:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/boroborome/labideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/boroborome/labideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.boroborome.labideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.boroborome.labideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
